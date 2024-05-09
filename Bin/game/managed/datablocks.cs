9335c877582efbdeb5b3ac7bf130d765de6385d5
15966019570 <2562750068wly@gmail.com>
2024-05-09 14:16:53 +0800

    添加过关更新地图功能

--- Bin/game/managed/datablocks.cs
+++ Bin/game/managed/datablocks.cs
@@ -194,4 +194,24 @@ $managedDatablockSet = new SimSet() {
       animationReverse = "0";
          canSaveDynamicFields = "1";
    };
+   new t2dImageMapDatablock(MiniGolfBackImageMap) {
+      imageName = "~/data/images/MiniGolfBack.dds";
+      imageMode = "CELL";
+      frameCount = "-1";
+      filterMode = "SMOOTH";
+      filterPad = "0";
+      preferPerf = "1";
+      cellRowOrder = "1";
+      cellOffsetX = "0";
+      cellOffsetY = "0";
+      cellStrideX = "0";
+      cellStrideY = "0";
+      cellCountX = "1";
+      cellCountY = "1";
+      cellWidth = "683";
+      cellHeight = "512";
+      preload = "1";
+      allowUnload = "0";
+      force16Bit = "0";
+   };
 };
